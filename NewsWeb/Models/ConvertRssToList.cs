a30dc32043000101dc9adadbdf9e135ad5f1dee7
unknown <20130376@st.hcmuaf.edu.vn>
2023-11-22 16:44:44 +0700

    convert Rss to Item

---
+++ NewsWeb/Models/ConvertRssToList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Web;
+using System.Xml;
+
+namespace NewsWeb.Models
+{
+    public class ConvertRssToList
+    {
+        static List<RssItem> GetRssItems(string rssFeedUrl)
+        {
+            List<RssItem> rssItems = new List<RssItem>();
+
+            // Sử dụng XmlReader để đọc RSS feed
+            using (XmlReader reader = XmlReader.Create(rssFeedUrl))
+            {
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                if (feed != null)
+                {
+                    // Lặp qua mỗi mục trong RSS feed
+                    foreach (SyndicationItem item in feed.Items)
+                    {
+                        RssItem rssItem = new RssItem
+                        {
+                            Title = item.Title?.Text,
+                            Description = item.Summary?.Text,
+                            Link = item.Links.FirstOrDefault()?.Uri?.ToString(),
+                            PublishDate = item.PublishDate.DateTime
+                        };
+
+                        rssItems.Add(rssItem);
+                    }
+                }
+            }
+
+            return rssItems;
+        }
+    }
+}
---
+++ NewsWeb/Models/RssItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsWeb.Models
+{
+    public class RssItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Link { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
