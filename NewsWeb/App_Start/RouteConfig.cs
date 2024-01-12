5f6d5b0a97daaebea5be072dc71fe8b365133523
HuynhAnhTai0207 <20130388@st.hcmuaf.edu.vn>
2024-01-12 20:54:31 +0700

    admin

--- NewsWeb/App_Start/RouteConfig.cs
+++ NewsWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace NewsWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "NewsWeb.Controllers" }
+
             );
         }
     }
---
+++ NewsWeb/Migrations/202401121340137_DbIni.cs
@@ -0,0 +1,16 @@
+﻿namespace NewsWeb.Models
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DbIni : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
