dd24d679a1310fc93c6913f9065af3365642a34c
unknown <20130376@st.hcmuaf.edu.vn>
2024-01-13 21:39:39 +0700

    complete mangae admin

--- NewsWeb/Areas/Admin/Controllers/AccoutManageController.cs
+++ NewsWeb/Areas/Admin/Controllers/AccoutManageController.cs
@@ -13,9 +13,14 @@ namespace NewsWeb.Areas.Admin.Controllers
         // GET: Admin/AccoutManager
         public ActionResult Account()
         {
-            List<Users> ItemList = db.Users.ToList();
-            ViewBag.ItemList = ItemList; 
-            return View();
+           // var loggedInUser = Session["LoggedInUser"] as NewsWeb.Models.Users;
+            //if (loggedInUser != null && loggedInUser.permission == "admin")
+           // {
+                List<Users> ItemList = db.Users.ToList();
+                ViewBag.ItemList = ItemList;
+                return View();
+            //}
+           // return Redirect("/Login/Login");
         }
         [HttpGet]
         public ActionResult Detail()
@@ -34,6 +39,38 @@ namespace NewsWeb.Areas.Admin.Controllers
                 
             }
             return View();
+        }
+        public ActionResult Update(Users user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Users userUpdate = db.Users.FirstOrDefault(c => c.id == user.id);
+                if (userUpdate != null)
+                {
+                    userUpdate.name = user.name;
+                    userUpdate.permission = user.permission;
+                    userUpdate.status = user.status;
+                    userUpdate.phone = user.phone;
+                    userUpdate.address = user.address;
+                    userUpdate.birthDay = user.birthDay;
+                    db.SaveChanges();
+                }
+            }
+
+            return Redirect("/Admin/AccoutManage/Detail?id=" + user.id);
+        }
+        public ActionResult Delete()
+        {
+            int id = int.Parse(Request["id"]);
+            var user = db.Users.FirstOrDefault(u => u.id == id);
+            if (user != null)
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+
+            return Redirect("/Admin/AccoutManage/Account");
         }
     }
 }
