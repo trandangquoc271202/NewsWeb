4a200e8b1ebe5ae029226238be84083b6bd6104e
unknown <20130376@st.hcmuaf.edu.vn>
2024-01-13 20:54:28 +0700

    create manage admin

---
+++ NewsWeb/Areas/Admin/Controllers/AccoutManageController.cs
@@ -0,0 +1,39 @@
+﻿using NewsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsWeb.Areas.Admin.Controllers
+{
+    public class AccoutManageController : Controller
+    {
+        Model1 db = new Model1();
+        // GET: Admin/AccoutManager
+        public ActionResult Account()
+        {
+            List<Users> ItemList = db.Users.ToList();
+            ViewBag.ItemList = ItemList; 
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Detail()
+        {
+            try
+            {
+                int id = int.Parse(Request["id"]);
+                Users user = db.Users.FirstOrDefault(c => c.id == id);
+                if(user != null)
+                {
+                    return View(user);
+                }
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
